fbe3926164792e1b8d67294f28f74cafdc16bfe2
pstgzh <1535147555@qq.com>
2018-03-18 21:19:32 +0800

    UI
    UI

---
+++ Assets/NumberController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberController : MonoBehaviour {
+    [SerializeField]
+    private Sprite[] sprite;
+    [SerializeField]
+    private Image bai;
+    [SerializeField]
+    private Image shi;
+    [SerializeField]
+    private Image ge;
+    public void chargeUI(int index)
+    {
+        if (index == 100)
+        {
+            bai.GetComponent<Image>().sprite = sprite[1];
+            shi.GetComponent<Image>().sprite = sprite[0];
+            ge.GetComponent<Image>().sprite = sprite[0];
+        }
+
+       else {
+            bai.GetComponent<Image>().sprite = sprite[0];
+            shi.GetComponent<Image>().sprite = sprite[index / 10];
+            ge.GetComponent<Image>().sprite = sprite[index % 10];
+            }
+        }
+}
