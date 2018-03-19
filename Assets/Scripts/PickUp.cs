0542166d8d1aed77f14831df40109d0e4a9eca27
pstgzh <30281885+pstgzh@users.noreply.github.com>
2018-03-19 12:09:21 +0800

    上传

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PickUp : MonoBehaviour {
+
+    /// <summary>
+    /// 抓取物体到手柄动画
+    /// </summary>
+    /// <param name="obj"></param>
+    public void PutObject(GameObject obj)
+    {
+        transform.DOMove(obj.transform.position, 2);
+    }
+
+    /// <summary>
+    /// 移动到指定位置动画
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="destination"></param>
+    public void MoveDestination(Transform destination)
+    {
+        transform.DOMove(destination.position, 2);
+
+    }
+}
